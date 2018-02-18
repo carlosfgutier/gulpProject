356c27dfb71e9f56234c22978006bcb61d58ab67
Carlos <carlosfgutier@gmail.com>
2018-02-18 11:01:59 -0600

    usig globs with sass

--- gulpFile.js
+++ gulpFile.js
@@ -5,8 +5,20 @@ gulp.task('hello', function() {
 	console.log("Hello Carlos");
 });
 
+
+// Direct sass input-output
+//------------------------------------------
+// gulp.task('sass', function() {
+// 	return gulp.src('app/scss/styles.scss')
+// 	.pipe(sass()) //using gulp sass
+// 	.pipe(gulp.dest('app/css'))
+// });
+
+
+// General input to specific output
+//------------------------------------------
 gulp.task('sass', function() {
-	return gulp.src('app/scss/styles.scss')
+	return gulp.src('app/scss/**/*.scss')
 	.pipe(sass()) //using gulp sass
 	.pipe(gulp.dest('app/css'))
 });
