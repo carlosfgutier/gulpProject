fc147b4651af1cb8d0b25593f12adacab53dc183
Carlos <carlosfgutier@gmail.com>
2018-02-22 15:09:27 -0600

    gulp browser reload

--- gulpFile.js
+++ gulpFile.js
@@ -5,7 +5,6 @@ gulp.task('hello', function() {
 	console.log("Hello Carlos");
 });
 
-
 // Direct sass input-output
 //------------------------------------------
 // gulp.task('sass', function() {
@@ -14,11 +13,44 @@ gulp.task('hello', function() {
 // 	.pipe(gulp.dest('app/css'))
 // });
 
-
+// GLOBBS
 // General input to specific output
 //------------------------------------------
+// gulp.task('sass', function() {
+// 	return gulp.src('app/scss/**/*.scss')
+// 	.pipe(sass())
+// 	.pipe(gulp.dest('app/css'))
+// });
+
+//WATCHING
+//------------------------------------------
+// Single watch
+// gulp.watch('app/scss/**/*.scss', ['sass']);
+
+// Multiple watch
+gulp.task('watch', ['browserSync', 'sass'], function() {
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	//Whatever other files I want to watch
+});
+
+// AUTOMATIC BROWSER RELOAD
+//------------------------------------------
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		},
+	});
+});
+
+// Update sass task to allow browserSync to update CSS
 gulp.task('sass', function() {
-	return gulp.src('app/scss/**/*.scss')
-	.pipe(sass()) //using gulp sass
+	return gulp.src('app/css/**/*.scss')
+	.pipe(sass())
 	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}));
 });
